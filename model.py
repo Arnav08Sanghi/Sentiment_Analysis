3bc962abcfbe9fb128152d093960f481fdd120a1
Arnav08Sanghi <sanghia@tcd.ie>
2025-05-11 18:17:53 +0530

    created a file for the tokenization of the reviews and putting it all in a function

--- model.py
+++ model.py
@@ -1,10 +1,15 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tokenization
 
 file = pd.read_csv("C:\\Users\\arnav\\OneDrive\\Desktop\\Coding Projects\\AI_PROJ_ONE\\IMDB_Dataset.csv")
 print(file)
 
 x_value = file["review"]
 y_value = file["sentiment"]
+
 
 print(x_value)
 print(y_value)
---
+++ tokenization.py
@@ -0,0 +1,21 @@
+import nltk
+import string
+from nltk.corpus import stopwords
+
+# Download required resources
+nltk.download('punkt')
+nltk.download('stopwords')
+
+
+# Tokenization pattern: Keep hashtags (#word) and URLs together, split everything else normally
+def custom_tokenize(text):
+    tokens = text
+
+    # Remove punctuation (except in hashtags and URLs, which are preserved by regex)
+    tokens = [word for word in tokens if word not in string.punctuation]
+
+    # Remove stopwords
+    list_stopwords = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word.startswith("#") or word.startswith("http") or word.lower() not in list_stopwords]
+
+    return tokens
