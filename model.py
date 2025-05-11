957b8357319ce9f249c3fb13cd0acfa5ba5f0c4f
Arnav08Sanghi <sanghia@tcd.ie>
2025-05-11 09:38:12 +0530

    reading the csv file and seperating the input and output columns

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+file = pd.read_csv("C:\\Users\\arnav\\OneDrive\\Desktop\\Coding Projects\\AI_PROJ_ONE\\IMDB_Dataset.csv")
+print(file)
+
+x_value = file["review"]
+y_value = file["sentiment"]
+
+print(x_value)
+print(y_value)
