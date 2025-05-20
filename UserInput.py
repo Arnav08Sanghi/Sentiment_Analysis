7d6edbbb5b8bc79b3d2f7a72f475f80b93b976bd
Arnav08Sanghi <sanghia@tcd.ie>
2025-05-21 00:16:07 +0530

    load the models and vectorizer into the userInput file

--- UserInput.py
+++ UserInput.py
@@ -7,4 +7,9 @@ vectorizer = load("tfidf_vectorizer")
 
 X_input = vectorizer.transform(user_input)
 
+
+best_model_rbf = load("rbf_svm_model_C")
+svm_model_rbf = load("rbf_svm_model")
+svm_model_linear = load("linear_svm_model")
+
 print(X_input)
