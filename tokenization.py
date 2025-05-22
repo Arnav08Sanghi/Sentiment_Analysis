942af1622a4aa360ed9ba6adfc0867a7d8bdb37c
Arnav08Sanghi <sanghia@tcd.ie>
2025-05-22 09:58:34 +0530

    remove unused code from files

--- tokenization.py
+++ tokenization.py
@@ -60,31 +60,3 @@ def make_numerical_vector (path):
 
 
 
-# import pandas as pd
-# import numpy as np
-
-# from tokenization import tokenize_words, make_numerical_vector
-    #file ["y-values"] = np.where(file["sentiment"] == 'positive', 1, 0)
-# def loading_data(path):
-#     file = pd.read_csv(path)
-#     #file = pd.read_csv("C:\\Users\\arnav\\OneDrive\\Desktop\\Coding Projects\\AI_PROJ_ONE\\IMDB_Dataset.csv")
-    
-#     file ["tokens"] = np.where(tokenize_words(file["review"]))
-#     x_value = np.where(make_numerical_vector(path))
-    
-#     #x_value = np.where(tokenize_words(file["review"]))
-#     #y_value = file["sentiment"]
-#     y_value = np.where(file["sentiment"] == 'positive', 1, 0)
-
-#     return x_value, y_value
-
-
-# #x_value = tokenization.make_numerical_vector(x_value)
-# #print(x_value)
-# #print(y_value)
-
-
-
-
-
-
